9a1ad9c2244f04934e378ae357f919ae9de66d67
sling254 <mwanikidenis7@gmail.com>
2022-01-03 19:40:28 +0300

    add Profile EditView and add Url to the view

--- hood/urls.py
+++ hood/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . views import IndexView, ProfileView
+from . views import IndexView, ProfileView,ProfileEditView
 
 urlpatterns = [
     path('',IndexView,name='index'),
     path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
+    path('profile/edit/<int:pk>/', ProfileEditView.as_view(), name='profile-edit'),
 ]
--- hood/views.py
+++ hood/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import View
 from .models import UserProfile
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, UpdateView, CreateView, DeleteView
 
 
 # Create your views here.
@@ -30,5 +32,21 @@ class ProfileView(View):
 
         return render(request, 'profile.html', context)
 
+
+class ProfileEditView(LoginRequiredMixin, UserPassesTestMixin,UpdateView):
+    model = UserProfile
+    fields = ['email', 'bio', 'birth_date', 'neighborhood', 'picture']
+    template_name = 'profile_edit.html'
+
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('profile', kwargs={'pk': pk})
+        
+
+    def test_func(self):
+        profile = self.get_object()
+        if self.request.user == profile.user:
+            return True
+        return False
 
 

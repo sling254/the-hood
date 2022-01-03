d7c0258741bd2c9cfc973b5509e16c382093f01c
sling254 <mwanikidenis7@gmail.com>
2022-01-03 17:23:28 +0300

    add UserProfile Model

--- hood/models.py
+++ hood/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import datetime as dt
+from django.db.models.deletion import CASCADE
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
@@ -26,3 +32,28 @@ class Business(models.Model):
 
   def __str__(self):
     return self.name
+
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,primary_key=True,verbose_name='user',related_name='profile', on_delete=models.CASCADE)
+    username = models.CharField(max_length=50,blank=True,null=True)
+    bio = models.TextField(max_length=500,blank=True,null=True)
+    birth_date = models.DateField(blank=True,null=True)
+    neighborhood  = models.CharField(max_length=50,blank=True,null=True)
+    picture = CloudinaryField('image',default='static/usericon.png',blank=True)
+    email = models.EmailField(max_length=50,blank=True,null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()

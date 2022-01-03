fd9019c9febeda3070e930aa93374654da21f8c8
sling254 <mwanikidenis7@gmail.com>
2022-01-03 19:18:26 +0300

    add profile view to render profile html

--- hood/views.py
+++ hood/views.py
@@ -11,3 +11,21 @@ def IndexView(request):
     }
     
     return render(request, 'index.html',context)
+
+
+
+class ProfileView(View):
+    def get(self, request, pk, *args, **kwargs):
+        profile = UserProfile.objects.get(pk=pk)
+        projects = Project.objects.filter(user=profile.user).all()
+        user = profile.user
+        
+        context = {
+            'user': user,
+            'profile': profile,
+        }
+
+        return render(request, 'profile.html', context)
+
+
+

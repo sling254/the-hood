fe498c749abb0949a0cf9d6cb9da945ab1d03976
sling254 <mwanikidenis7@gmail.com>
2022-01-04 09:36:49 +0300

    refactor base html and add onboarding html

--- hood/urls.py
+++ hood/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . views import IndexView, ProfileView,ProfileEditView
+from . views import IndexView, ProfileView,ProfileEditView,OnboardingView
 
 urlpatterns = [
     path('',IndexView,name='index'),
+    path('onboarding',OnboardingView,name='onboarding'),
     path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
     path('profile/edit/<int:pk>/', ProfileEditView.as_view(), name='profile-edit'),
 ]

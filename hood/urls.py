df62bacd08428c8b30efb1ca9989bdd0de6609e2
sling254 <mwanikidenis7@gmail.com>
2022-01-05 09:56:05 +0300

    add create post form and a url to it

--- hood/urls.py
+++ hood/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . views import IndexView, ProfileView,ProfileEditView,OnboardingView,BusinessSearch
+from . views import IndexView, ProfileView,ProfileEditView,OnboardingView,BusinessSearch, create_post
 
 urlpatterns = [
     path('',IndexView,name='index'),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
     path('profile/edit/<int:pk>/', ProfileEditView.as_view(), name='profile-edit'),    
     path('business/search/', BusinessSearch.as_view(),  name='business-search'),
+    path('create_post/<int:neighborhood_id>/',create_post,name='create_post'),
 ]

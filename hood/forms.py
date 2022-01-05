cde5b8d878d0b65d330212afad0eb3ef1cea0433
sling254 <mwanikidenis7@gmail.com>
2022-01-05 11:18:50 +0300

    refactor forms.py

--- hood/forms.py
+++ hood/forms.py
@@ -5,6 +5,19 @@ from django.forms import fields
 from .models import Post
 
 class CreatePostForm(forms.ModelForm):
+  title = forms.CharField(
+    label='',
+    widget=forms.TextInput(attrs={
+      'placeholder': 'Title'
+      }))
+  post = forms.CharField(
+    label='',
+    widget=forms.Textarea(attrs={
+      'placeholder': 'Say something...',
+      'rows': '4',
+      'class': 'form-control mt-2',
+      }))
+  
   class Meta:
     model = Post
     fields = ('title','post','image')
